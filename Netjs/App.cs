c29c00d42c435776b17b600f47f8d41a13aed8aa
Frank A. Krueger <fak@praeclarum.org>
2016-12-08 15:28:33 -0800

    Fix assembly resolution for PCLs

--- Netjs/App.cs
+++ Netjs/App.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2014 Frank A. Krueger
+﻿// Copyright 2014-2016 Frank A. Krueger
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,8 +25,6 @@ namespace Netjs
 {
 	public class App : IAssemblyResolver
 	{
-		string asmDir = "";
-
 		class Config
 		{
 			public string MainAssembly = "";
@@ -61,7 +59,7 @@ namespace Netjs
 		void Run (Config config)
 		{
 			if (config.ShowHelp) {
-				Console.WriteLine ("Netjs compiler, Copyright 2014 Frank A. Krueger");
+				Console.WriteLine ("Netjs compiler, Copyright 2014-2016 Frank A. Krueger");
 				Console.WriteLine ("netjs [options] assembly-file");
 				Console.WriteLine ("   -help                Lists all compiler options (short: -?)");
 				return;
@@ -72,18 +70,18 @@ namespace Netjs
 			}
 
 			var asmPath = Path.GetFullPath (config.MainAssembly);
-			asmDir = Path.GetDirectoryName (asmPath);
+			asmSearchPaths.Add (Path.GetDirectoryName (asmPath));
+
 			var outPath = Path.ChangeExtension (asmPath, ".ts");
 
 			Step ("Reading IL");
-			var parameters = new ReaderParameters {
-				AssemblyResolver = this,
-			};
-			var asm = AssemblyDefinition.ReadAssembly (asmPath, parameters);
-			mscorlib = AssemblyDefinition.ReadAssembly (typeof(String).Assembly.Location, parameters);
-			system = AssemblyDefinition.ReadAssembly (typeof(INotifyPropertyChanged).Assembly.Location, parameters);
-			systemCore = AssemblyDefinition.ReadAssembly (typeof(Enumerable).Assembly.Location, parameters);
-			systemDrawing = AssemblyDefinition.ReadAssembly (typeof(System.Drawing.Bitmap).Assembly.Location, parameters);
+			globalReaderParameters.AssemblyResolver = this;
+			globalReaderParameters.ReadingMode = ReadingMode.Immediate;
+
+			var libDir = Path.GetDirectoryName (typeof (String).Assembly.Location);
+			asmSearchPaths.Add (libDir);
+			asmSearchPaths.Add (Path.Combine (libDir, "Facades"));
+			var asm = AssemblyDefinition.ReadAssembly (asmPath, globalReaderParameters);
 
 			Step ("Decompiling IL to C#");
 			var context = new DecompilerContext (asm.MainModule);
@@ -99,10 +97,18 @@ namespace Netjs
 			context.Settings.FullyQualifyAmbiguousTypeNames = true;
 			context.Settings.YieldReturn = false;
 			var builder = new AstBuilder (context);
+			var added = new HashSet<string> ();
 			builder.AddAssembly (asm);
-			foreach (var a in referencedAssemblies.Values) {
-				if (a != null)
+			added.Add (asm.FullName);
+			for (;;) {
+				var a = referencedAssemblies.Values.FirstOrDefault (x => !added.Contains (x.FullName));
+				if (a != null) {
 					builder.AddAssembly (a);
+					added.Add (a.FullName);
+				}
+				else {
+					break;
+				}
 			}
 			builder.RunTransformations ();
 
@@ -117,6 +123,8 @@ namespace Netjs
 
 			Step ("Done");
 		}
+
+		#region Logging
 
 		public static void Step (string message)
 		{
@@ -148,60 +156,52 @@ namespace Netjs
 		{
 			Console.WriteLine (format, args);
 		}
-			
+
+		#endregion
+
 		#region IAssemblyResolver implementation
 
-		AssemblyDefinition mscorlib;
-		AssemblyDefinition system;
-		AssemblyDefinition systemCore;
-		AssemblyDefinition systemDrawing;
-
+		readonly ReaderParameters globalReaderParameters = new ReaderParameters ();
+		readonly List<string> asmSearchPaths = new List<string> ();
 		readonly Dictionary<string, AssemblyDefinition> referencedAssemblies = new Dictionary<string, AssemblyDefinition> ();
 
 		public AssemblyDefinition Resolve (AssemblyNameReference name)
 		{
-			return Resolve (name, null);
+			//Info ("R1: {0}", name);
+			return Resolve (name, globalReaderParameters);
 		}
 		public AssemblyDefinition Resolve (AssemblyNameReference name, ReaderParameters parameters)
 		{
-			switch (name.Name) {
-			case "mscorlib":
-				return mscorlib;
-			case "System":
-				return system;
-			case "System.Core":
-				return systemCore;
-			case "System.Drawing":
-				return systemDrawing;
-			default:
-				var n = name.Name;
-				AssemblyDefinition asm;
-				if (!referencedAssemblies.TryGetValue (n, out asm)) {
+			//Info ("R2: {0}", name);
+			var n = name.Name;
+			AssemblyDefinition asm;
+			if (!referencedAssemblies.TryGetValue (n, out asm)) {
+				foreach (var asmDir in asmSearchPaths) {
 					var fn = Path.Combine (asmDir, name.Name + ".dll");
 					if (File.Exists (fn)) {
-
-						asm = parameters != null ? 
-							AssemblyDefinition.ReadAssembly (fn, parameters) : 
-							AssemblyDefinition.ReadAssembly (fn);
+						asm = AssemblyDefinition.ReadAssembly (fn, parameters);
+						referencedAssemblies[n] = asm;
 						Info ("  Loaded {0}", fn);
+						break;
 					}
-					else {
-						asm = null;
-						Error ("  Could not find assembly {0}", name);
-					}
-					referencedAssemblies [n] = asm;
 				}
-				return asm;
+				if (asm == null) {
+					Error ("  Could not find assembly {0}", name);
+				}
 			}
+			return asm;
 		}
 		public AssemblyDefinition Resolve (string fullName)
 		{
+			//Info ("R3: {0}", fullName);
 			return null;
 		}
 		public AssemblyDefinition Resolve (string fullName, ReaderParameters parameters)
 		{
+			//Info ("R4: {0}", fullName);
 			return null;
 		}
+
 		#endregion
 	}
 }
